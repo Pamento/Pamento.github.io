e818246afe77e27c36f325137f51465691d6924b
Pamento <pamento.dev@gmail.com>
2020-11-03 12:14:04 +0100

    git rebase continue: add js/script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onscroll = function() {stickyAHead()};
+
+var header = document.getElementById("stickyHeader");
+var sticky = header.offsetTop;
+
+function stickyAHead() {
+    console.log("offsetTop: "+window.pageYOffset);
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
